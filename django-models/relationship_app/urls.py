ed201759005e94923ae9fdb2cd7c0fb6bed8d5e9
fuad <fuadbinseid@gmail.com>
2024-08-18 23:46:09 +0300

    djmodel

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+    path(path('register/', RegisterView.as_view(), name='register'), RegisterView.as_view(), name='register'),
 ]
