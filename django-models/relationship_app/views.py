3763eece30bcd4fcfc85a60b72210986f64f4e0a
fuad <fuadbinseid@gmail.com>
2024-08-18 23:26:44 +0300

    djmodel

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
 
+
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
