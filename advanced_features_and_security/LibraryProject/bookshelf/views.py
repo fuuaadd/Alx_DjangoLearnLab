f66f337328ba81e3f2d448b1dc00611b3fbdc3e6
fuad <fuadbinseid@gmail.com>
2024-08-25 22:44:45 +0300

    advanced

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import get_object_or_404, render, redirect
 from .models import Document
+ ["book_list", "books"]
 
 @permission_required('relationship_app.can_edit', raise_exception=True)
 def edit_document(request, document_id):
