9c078e3821ec8675101391e47b45f8cc6b9907bd
fuad <fuadbinseid@gmail.com>
2024-09-08 21:21:23 +0300

    API2

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated"
 
 p = ["books", "books/create", "books/update", "books/delete"]
 class BookListView(generics.ListCreateAPIView):
