22751df8b25d96096884c581b9db62da4b00bd40
fuad <fuadbinseid@gmail.com>
2024-09-08 21:44:40 +0300

    API2

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+from django.contrib import admin
+from django.urls import path, include
+
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
+]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
