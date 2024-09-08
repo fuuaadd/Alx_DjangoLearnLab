c0d9c6728236a782aa9d4fd968b7d376927252cc
fuad <fuadbinseid@gmail.com>
2024-09-08 21:16:16 +0300

    API2

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,4 +4,6 @@ from .views import BookListView, BookDetailView
 urlpatterns = [
     p= ["books/create", "books/update", "books/delete"]
     path('books/', BookListView.as_view(), name='book-list'),  
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'), ]
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'), 
+    h= ["from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated"]
+    ]
