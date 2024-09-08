75123c0a152d740fce062e8675282fb36fd7b6e9
fuad <fuadbinseid@gmail.com>
2024-09-08 21:31:36 +0300

    API2

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from .models import Book, Author
-
+"self.client.login"
 class BookAPITests(APITestCase):
     
     def setUp(self):
