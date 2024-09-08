bc301c869612ec7ddac8bd0dfad95f4d925c9b84
fuad <fuadbinseid@gmail.com>
2024-09-08 21:18:21 +0300

    API2

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
-from .views import BookListView, BookDetailView
-["from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated"]
-urlpatterns = [
-    p= ["books/create", "books/update", "books/delete"]
-    path('books/', BookListView.as_view(), name='book-list'),  
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'), 
-    h= ["from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated"]
-    ]
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    # Allows any user to view, but only authenticated users can modify
+    permission_classes = [IsAuthenticatedOrReadOnly]
