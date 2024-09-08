48c12894deae4d55e24856b92fa96d1eabc2a9c9
fuad <fuadbinseid@gmail.com>
2024-09-08 21:48:18 +0300

    API2

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -82,6 +82,14 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 
 # Password validation
