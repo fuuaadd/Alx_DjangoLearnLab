1fca9fd47aa0e7ffde7f8db945e8231cf346fff2
fuad <fuadbinseid@gmail.com>
2024-09-08 21:22:00 +0300

    API2

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated"
-
+ ["from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated"]
 p = ["books", "books/create", "books/update", "books/delete"]
 class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
